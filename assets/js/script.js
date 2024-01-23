931390c5b99370e42bc8921d92094b375bee0715
kevinoh45TuD <>
2024-01-23 03:37:29 +0000

    Code: generateList() function added to make an array of deck

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,53 @@ function clickInfo() {
     alert("Info");
 }
 
+let fullDeck = [];
+
+function generateList() {
+
+    fullDeck = [];
+
+    const hearts = [
+        "2-heart",
+        "3-heart",
+        "4-heart",
+        "5-heart"
+    ];
+
+    const diamonds = [
+        "2-diamond",
+        "3-diamond",
+        "4-diamond",
+        "5-diamond"
+    ];
+
+    const spades = [
+        "2-spade",
+        "3-spade",
+        "4-spade",
+        "5-spade"
+    ];
+
+    const clubs = [
+        "2-club",
+        "3-club",
+        "4-club",
+        "5-club"
+    ];
+
+    for (let i = 0; i <= 3; i++) {
+        fullDeck.push(hearts[i]);
+        fullDeck.push(spades[i]);
+        fullDeck.push(diamonds[i]);
+        fullDeck.push(clubs[i]);
+    }
+
+    console.log(fullDeck);
+}
+
 function spawnCards() {
+
+    generateList();
 
     let board = document.getElementById("main-game");
 
