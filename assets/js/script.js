596488c4db9feb4a9f48de6e919bee5a928fb0b0
kevinoh45TuD <>
2024-01-23 03:04:27 +0000

    Code: intial function for spawning cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,27 @@ function clickPlay() {
 function clickInfo() {
     alert("Info");
 }
+
+function spawnCards() {
+
+    let board = document.getElementById("main-game");
+
+    for (let i = 0; i <= 3; i++) {
+        console.log(i);
+
+        let card = document.createElement("img");
+        card.src = "assets/images/spade-icon.png";
+        card.id = "card" + i;
+
+        card.style.position = "fixed";
+        card.style.width = "4rem";
+
+        let calY = 15 + (8 * i);
+        let calX = 2.5 + (6 * i);
+
+        card.style.top = calY + "rem";
+        card.style.left = calX + "rem";
+
+        board.appendChild(card);
+    }
+}
